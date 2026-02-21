3d7ac1f54528d3c8b343b0f970783054da770f3b
Daniil Surin <wkingsurin@gmail.com>
2026-02-21 19:36:09 +0500

    feat, style: Add from

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
 import "./App.css";
 
 export default function App() {
-	return <></>;
+	return (
+		<>
+			<div className="container">
+				<div className="box">
+					<form action="POST" className="form">
+						<h2 className="title">Sign in</h2>
+						<div className="form-content">
+							<label htmlFor="email" className="label">
+								Email:
+								<input type="text" id="email" placeholder="email@example.com" />
+							</label>
+							<label htmlFor="password" className="label">
+								Password:
+								<input type="text" id="password" placeholder="Password" />
+							</label>
+							<button type="submit" className="btn">
+								Enter
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</>
+	);
 }
